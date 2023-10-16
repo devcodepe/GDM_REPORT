ca3311edd8fc51db082e9abf99ef86cb80d56b5e
devcodepe <77082186+devcodepe@users.noreply.github.com>
2023-10-16 18:32:46 -0500

    creation

---
+++ Controllers/Dashboard.php
@@ -0,0 +1,19 @@
+<?php 
+
+	class Dashboard extends Controllers{
+		public function __construct()
+		{
+			parent::__construct();
+		}
+
+		public function dashboard()
+		{
+			$data['page_id'] = 2;
+			$data['page_tag'] = "Dashboard - GeekDentalReport";
+			$data['page_title'] = "Dashboard - GeekDentalReport";
+			$data['page_name'] = "Dashboard";
+			$this->views->getView($this,"dashboard",$data);
+		}
+
+	}
+ ?>
