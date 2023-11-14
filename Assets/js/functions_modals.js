9448a30bc559f67b45f843059672e065192735a2
DevAaron2023 <dev.aaron0197@gmail.com>
2023-11-13 20:35:56 -0500

    Creacion de Tabla Roles

--- Assets/js/functions_modals.js
+++ Assets/js/functions_modals.js
@@ -1,3 +1,11 @@
+$(function () {
+  $("#roltable").DataTable({
+    "responsive": true, "lengthChange": false, "autoWidth": false,
+    "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+  }).buttons().container().appendTo('#roltable_wrapper .col-md-6:eq(0)');
+  
+});
+
 function openModal(){
     $('#modalFormRol').modal('show');
 }
